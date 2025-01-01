e2fb610b657d63489b98e34607ad8a4959a134c9
Andrew Glines <aglines@gmail.com>
2025-01-01 16:35:28 +0100

    edit findhost, update readme

--- src/getdata_findhost.py
+++ src/getdata_findhost.py
@@ -1,22 +1,32 @@
 import requests
 import json
-from config.private_data import API_SOURCE_URL_HOSTS
+from config.private_data import API_SOURCE_URL_HOSTS, API_SOURCE_URL_TIMESLOTS
 
-url = API_SOURCE_URL_HOSTS
-names_ids = {}
+# Goal:  Find the specific DJ ID from the API data
+# This code will be only run once, so no need to make it a function
+# and minimal error handling is needed
 
-# get the list of hostIDs to identify the specific DJ
-def get_host_ids():
-    response = requests.get(url)
-    if response.status_code not in range(200,201):
-        print(f"Error in API request response: {response.status_code}")
-    response=response.json()
-    data=response['results']
-    for i in data:
-        names_ids[i['name']] = i['id']
+# First, look at the list of hostIDs and names
     
-    print(names_ids)
+url = API_SOURCE_URL_HOSTS
+response = requests.get(url)
+response=response.json()
 
-get_host_ids()
+for i in response['results']:
+    print(i['name'], i['id'])
 
+# The DJ's name doesn't immediately appear in the list of host names
+# We could hunt through a full list of responses
+# But we also know the show name and its timeslot
+# so maybe the timeslots API has the info we need
+
+url = API_SOURCE_URL_TIMESLOTS
+response = requests.get(url)
+response=response.json()
+
+for i in response['results']:
+    print(i['program_name'], i['host_names'], i['hosts'])
+
+# Got it. The DJ's name is in the timeslots API response
+# The hostID for this DJ is 26
 
